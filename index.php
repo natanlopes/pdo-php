f51231698de46db1c3f3785202dec9fcd396c7cb
natanlopes <natan1918@gmail.com>
2022-03-27 11:30:04 -0300

    Connecting to MySQL
    onnections are established by creating instances of the PDO base class. It doesn't matter which driver you want to use; you always use the PDO class name. The constructor accepts parameters for specifying the database source (known as the DSN) and optionally for the username and password (if any).

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-$conexao = new PDO('mysql:host=localhost;dbname=pdo', "root", "");
+try {
+    $conexao = new PDO('mysql:host=localhost;dbname=pdo', "root", "");
+} catch (PDOException $e) {
+   echo "An error has occurred:". $e->getMessage() . "<br/>";
+}
+
 ?>
